0def909206df314527336e4cdfa8ec8e4e4d13d3
alexcapricorngmailcom <107393171+alexcapricorngmailcom@users.noreply.github.com>
2023-07-16 19:42:05 +0300

    test styled components

--- src/page-content/Home/index.tsx
+++ src/page-content/Home/index.tsx
@@ -1,7 +1,15 @@
+import { styled } from "@mui/material/styles"
 import { colors } from "../../const/colors"
 
 export const Home = () => {
     return (
-        <main style={{color: colors.secondary}}>Home</main>
+        <main style={{color: colors.secondary}}>
+            <Test>Im tomato</Test>
+            Home</main>
     )
 }
+
+const Test = styled('div')`
+    color: tomato;
+`;
+
