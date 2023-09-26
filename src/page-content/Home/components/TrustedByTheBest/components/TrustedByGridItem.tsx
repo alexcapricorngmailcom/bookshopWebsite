266d5d6ab0db713b65a6425c24551a0ed3dc0d92
alexcapricorngmailcom <107393171+alexcapricorngmailcom@users.noreply.github.com>
2023-09-26 15:21:43 +0300

    update About => TrustedByTheBest

--- src/page-content/Home/components/TrustedByTheBest/components/TrustedByGridItem.tsx
+++ src/page-content/Home/components/TrustedByTheBest/components/TrustedByGridItem.tsx
@@ -1,3 +1,4 @@
+import { styled } from '@mui/material/styles';
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 
@@ -5,15 +6,30 @@ import { HeadingH5, Paragraph } from "../../../../../design-system/typography";
 
 export const TrustedByGridItem = ({img, alt, title, paragraph}: BookDetailsProps) => {
     return (
-        <Grid item lg={3} sm={6} xs={12} textAlign='center'>
+        <StyledGrid item lg={3} md={6} sm={6} xs={12} textAlign='center'>
             <Box width='100%' height='90px'>
                 <img src={img} alt={alt} style={{width:'100%', height:'100%', objectFit:'contain'}}/>
             </Box>
             <HeadingH5 sx={{mt:'20px'}}>{title}</HeadingH5>
-            <Paragraph sx={{mt:'10px'}}>{paragraph}</Paragraph>
-        </Grid>
+            <StyledParagraph sx={{mt:'10px'}}>{paragraph}</StyledParagraph>
+        </StyledGrid>
     );
 }
+
+const StyledGrid = styled(Grid)`
+    @media (max-width: 1199px) {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+`;
+
+const StyledParagraph = styled(Paragraph)`
+    @media (max-width: 1199px) {
+        max-width: 295px;
+    }
+`;
+
 
 type BookDetailsProps = {
     img: string;
