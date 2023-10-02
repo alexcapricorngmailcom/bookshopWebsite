802849480c5ba7310845f7b323e9e38862a5c830
alexcapricorngmailcom <107393171+alexcapricorngmailcom@users.noreply.github.com>
2023-10-02 15:21:53 +0300

    add @media GetBookCopyToday

--- src/page-content/Home/components/GetBookCopyToday/index.tsx
+++ src/page-content/Home/components/GetBookCopyToday/index.tsx
@@ -15,17 +15,19 @@ export const GetBookCopyToday = () => {
         <StyledSection>
             <Container maxWidth='lg'>
                 <Grid container spacing={12} alignItems='center'>
-                    <Grid item md={6} sm={12}>
+                    <StyledGriditem1 item lg={6} md={12} sm={12} xs={12}>
                         <HeadingH3 sx={{color: WHITE}}>Get Book Copy Today!</HeadingH3>
                         <LineLarge sx={{mt:'20px'}} />
-                        <Paragraph sx={{mt:'20px', textAlign:'justify', color: PARAGRAPH_SECONDARY}}>There are many variations of passages of Lorem available, but the majority have suffered alteration in some form.</Paragraph>
+                        <StyledParagraph>There are many variations of passages of Lorem available, but the majority have suffered alteration in some form.</StyledParagraph>
                         <Box mt='30px'>
                             <ButtonAction component={RouterLink} to='/myStore'>Order Today</ButtonAction> 
                         </Box>
-                    </Grid>
-                    <Grid item md={6} sm={12}>
-                        <img src={getBookCopyToday} style={{width:'100%', height:'100%'}} alt="callout" />
-                    </Grid>
+                    </StyledGriditem1>
+                    <StyledGriditem2 item lg={6} md={12} sm={12} xs={12}>
+                        <StyledImageBox>
+                            <img src={getBookCopyToday} style={{width:'100%', height:'100%'}} alt="callout" />
+                        </StyledImageBox>
+                    </StyledGriditem2>
                 </Grid>
             </Container>
         </StyledSection>
@@ -35,4 +37,39 @@ export const GetBookCopyToday = () => {
 const StyledSection = styled('section')`
     padding: 40px 0px;
     background-color: ${MAIN};
+
+    @media (max-width: 1199px) {
+        padding: 145px 0px;
+    }
+`;
+
+const StyledGriditem1 = styled(Grid)`
+    
+    @media (max-width: 1199px) {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+    }
+`;
+
+const StyledParagraph = styled(Paragraph)`
+    margin-top: 20px;
+    text-align: justify;
+    color: ${PARAGRAPH_SECONDARY};
+
+    @media (max-width: 1199px) {
+        text-align: center;
+    }
+`;
+
+const StyledGriditem2 = styled(Grid)`
+    padding-top: 40px !important;
+`;
+
+const StyledImageBox = styled(Box)`
+    @media (max-width: 1199px) {
+        max-width: 540px;
+        margin: 0 auto;
+    }
 `;
