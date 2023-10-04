4583c92ae9129f378f07b15b0fe938db06ef284b
alexcapricorngmailcom <107393171+alexcapricorngmailcom@users.noreply.github.com>
2023-10-04 09:28:14 +0300

    fix bugs in Licenses

--- src/page-content/Licenses/components/LicensesItem/index.tsx
+++ src/page-content/Licenses/components/LicensesItem/index.tsx
@@ -1,7 +1,7 @@
 import { styled } from '@mui/material/styles';
 import Container from '@mui/material/Container';
 
-import { WHITE } from '../../../../design-system/colors';
+import { MAIN, WHITE } from '../../../../design-system/colors';
 import { HeadingH4, HeadingH6, Paragraph } from '../../../../design-system/typography';
 
 export const LicensesItem = () => {
@@ -9,13 +9,13 @@ export const LicensesItem = () => {
         <StyledSection>
             <Container maxWidth='lg'>
                 <HeadingH4>Photography</HeadingH4>
-                <Paragraph sx={{mt:'10px'}}>All images used in the <HeadingH6 sx={{display:'inline-block'}}>Pages</HeadingH6> Webflow Template are licensed for free personal and commercial use. If you'd like to use any specific image, you can check the licenses and download the images for free on <HeadingH6 sx={{display:'inline-block'}}>Unsplash, Pexels.</HeadingH6></Paragraph>
+                <Paragraph sx={{mt:'10px'}}>All images used in the <StyledSpan>Pages</StyledSpan> Webflow Template are licensed for free personal and commercial use. If you'd like to use any specific image, you can check the licenses and download the images for free on <StyledSpan>Unsplash, Pexels.</StyledSpan></Paragraph>
                 <HeadingH6 sx={{mt:'40px'}}>Pexels:</HeadingH6>
                 <Paragraph sx={{mt:'10px'}}>Image 1, Image 2, Image 3, Image 4, Image 5, Image 6, Image 7, Image 8</Paragraph>
                 <HeadingH6 sx={{mt:'40px'}}>Unsplash:</HeadingH6>
                 <Paragraph sx={{mt:'10px'}}>Image 1, Image 2, Image 3</Paragraph>
                 <HeadingH4 sx={{mt:'40px'}}>Fonts</HeadingH4>
-                <Paragraph sx={{mt:'10px'}}>Pages template uses free licensed <HeadingH6 sx={{display:'inline-block'}}>Google Fonts.</HeadingH6> Please check <HeadingH6 sx={{display:'inline-block'}}>Cardo</HeadingH6> and <HeadingH6 sx={{display:'inline-block'}}>Inter.</HeadingH6></Paragraph>
+                <Paragraph sx={{mt:'10px'}}>Pages template uses free licensed <StyledSpan>Google Fonts.</StyledSpan> Please check <StyledSpan>Cardo</StyledSpan> and <StyledSpan>Inter.</StyledSpan></Paragraph>
             </Container>
         </StyledSection>
     );
@@ -28,4 +28,13 @@ const StyledSection = styled('section')`
     @media (max-width: 599px) {
         padding: 125px 0px;
     }
+`;
+
+const StyledSpan = styled('span')`
+    font-family: Cardo, serif;
+    font-size: 22px;
+    font-weight: 700;
+    line-height: 130%;
+    letter-spacing: -0.015em;
+    color: ${MAIN};
 `;
