61d5f555419ea2f838c7d5f844c0b771368851fc
alexcapricorngmailcom <107393171+alexcapricorngmailcom@users.noreply.github.com>
2023-10-07 12:40:22 +0300

    add webflow https

--- src/shared/components/Footer/index.tsx
+++ src/shared/components/Footer/index.tsx
@@ -122,9 +122,8 @@ export const Footer = () => {
                 <Box sx={{mt: "25px", textAlign: 'center'}}>
                     <Paragraph sx={{display:'inline', color: '#B4C7E7'}}>&#169; Drafted by </Paragraph>  
                     <Link href='https://www.victorflow.com/' target='_blank' rel="noopener" underline='none'> <Paragraph sx={{display:'inline', color: WHITE }}>Victorflow</Paragraph></Link>
-                    <Paragraph sx={{display:'inline', color: '#B4C7E7'}}> - Powered by </Paragraph>  
-                    <Paragraph sx={{display:'inline', color:WHITE}}>Webflow</Paragraph>
-                    
+                    <Paragraph sx={{display:'inline', color: '#B4C7E7'}}> - Powered by </Paragraph>
+                    <Link href='https://webflow.com/' target='_blank' rel="noopener" underline='none'> <Paragraph sx={{display:'inline', color: WHITE }}>Webflow</Paragraph></Link>
                 </Box>
             </Container>
         </StyledFooter>
